74aefdc25c2ff6a4508b5679829e543acfbab625
PolinaDev1 <Poli20001808@gmail.com>
2025-04-16 18:32:23 +0300

    Add files via upload

---
+++ Урок14ДЗ1.py
@@ -0,0 +1,3 @@
+def restaurant(surname, number_of_people=2):
+    print(f"Цей стіл заброньовано для призвища {surname} для {number_of_people} людей")
+restaurant("Коплик")
---
+++ Урок14ДЗ2.py
@@ -0,0 +1,4 @@
+def restaurant(surname, number_of_people=2):
+    print(f"Цей стіл заброньовано для призвища {surname} для {number_of_people} людей")
+restaurant('Коплик')
+restaurant('Борисовa', 4)
